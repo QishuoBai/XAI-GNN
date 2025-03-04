0bbca573178582866db55d1daa261f8dabc8b7e5
QishuoBai <qsbai@foxmail.com>
2025-03-04 17:40:03 +0800

    12

--- frontend/src/components/InfoView.vue
+++ frontend/src/components/InfoView.vue
@@ -1,29 +1,120 @@
 <template>
-    <div class="h-100 w-100 pa-2 d-flex flex-column">
+  <div class="h-100 w-100 pa-2 d-flex flex-column">
     <div
       class="text-body-1 font-weight-bold d-flex flex-row justify-space-between align-end"
     >
       <div>Analysis Assistant</div>
-      <div class="text-caption">Powered by LLM</div>
+      <div class="text-caption">Powered by DeepSeek</div>
     </div>
     <v-divider></v-divider>
-    <div class="flex-grow-1 overflow-y-auto" style="height: 0px;">
-        <div class="bg-grey-lighten-1" style="height: 1000px;">
-            <!-- chat content -->
+    <div class="flex-grow-1 overflow-y-auto" style="height: 0px">
+      <!-- chat content -->
+      <div
+        v-for="(message, index) in messages"
+        :key="index"
+        :class="
+          'd-flex flex-row align-center w-100 ' +
+          (message.role === 'assistant' ? 'justify-start' : 'justify-end')
+        "
+      >
+        <div
+          v-if="message.role === 'user' || message.role === 'system'"
+          class="text-body-2 bg-grey-lighten-3 pa-2 rounded-xl mt-2"
+          style="max-width: 90%"
+        >
+          {{ message.content }}
         </div>
+        <div
+          v-if="message.role === 'assistant'"
+          class="text-body-2 bg-grey-lighten-3 pa-2 rounded-xl mt-2"
+          style="max-width: 90%"
+        >
+          {{ message.content }}
+        </div>
+      </div>
     </div>
-    <div class="bg-grey-lighten-5 rounded" style="min-height: 70px;">
-            <!-- input dialog -->
+    <div class="pa-1" style="min-height: 90px">
+      <!-- input dialog -->
+      <div
+        class="border elevation-1 h-100"
+        style="border-radius: 24px; padding: 12px"
+      >
+        <textarea
+          v-model="input_text"
+          type="text"
+          class="text-body-2"
+          style="
+            min-height: 60px;
+            width: 100%;
+            resize: none;
+            overflow: auto;
+            outline: none;
+          "
+          placeholder="ask anything"
+        ></textarea>
+        <div class="d-flex flex-row justify-space-between mt-1">
+          <div class="h-100 d-flex flex-row align-center">
+            <v-btn class="rounded-pill" @click="swich_model" color="black">
+              {{ models[selected_model].label }}
+              <v-icon icon="mdi-sync" end></v-icon>
+            </v-btn>
+          </div>
+          <div class="h-100 d-flex flex-row align-center">
+            <v-btn
+              :size="35"
+              icon="mdi-arrow-up"
+              @click="send"
+              color="black"
+              :loading="loading"
+            ></v-btn>
+          </div>
         </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import { deepseekRequest } from "@/utils";
 // Define the component
 export default {
-    name: 'InfoView',
-    // Your component's options go here
+  name: "InfoView",
+  // Your component's options go here
+  data() {
+    return {
+      input_text: "",
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      models: [
+        { label: "DeepSeek-V3", value: "deepseek-chat" },
+        { label: "DeepSeek-R1", value: "deepseek-reasoner" },
+      ],
+      selected_model: 0,
+      loading: false,
+    };
+  },
+  methods: {
+    send() {
+      if (this.input_text === "") {
+        return;
+      }
+      this.loading = true;
+      this.messages.push({ role: "user", content: this.input_text });
+      this.input_text = "";
+      deepseekRequest(
+        this.messages,
+        this.models[this.selected_model].value
+      ).then((completion) => {
+        this.messages.push({
+          role: "assistant",
+          content: completion.choices[0].message.content,
+        });
+        this.loading = false;
+      });
+    },
+    swich_model() {
+      this.selected_model = (this.selected_model + 1) % this.models.length;
+    },
+  },
 };
 </script>
 
--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -31,3 +31,31 @@ export async function getRequest(api_suffix){
         return error.response;
     }
 }
+
+import OpenAI from 'openai';
+
+const deepseek = new OpenAI({
+    baseURL: 'https://api.deepseek.com',
+    apiKey: 'sk-8ba2fc7ed5134e398b0bb030ac9c7412',
+    dangerouslyAllowBrowser: true,
+});
+
+export async function deepseekRequest(messages, model) {
+    // messages 是一个数组，每个元素是一个对象
+    // 每个对象有一个 key 是 role，值是 system 或者 user 或者 assistant
+    // 另一个 key 是 content，值是字符串
+    // 例如 [{ role: "system", content: "You are a helpful assistant." }, {role: 'user', content: '你好'}, {role: 'assistant', content: '你好'}]
+    try {
+        // 使用 axios 发送 POST 请求
+        const completion = await deepseek.chat.completions.create({
+            messages: messages,
+            model: model,
+          });
+        console.log('deepseekRequest 成功: ', completion);
+        return completion;
+        
+    } catch (error) {
+        console.log('deepseekRequest 失败: ', error);
+        return error;
+    }
+}
